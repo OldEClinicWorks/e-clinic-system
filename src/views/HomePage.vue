f7c35090f0138c959b9321ca8f2f08b5cfc05ad0
dev global <salarAdelSabry@gmail.com>
2023-08-19 11:04:06 +0300

    ok

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,7 +2,7 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Secretary App 2</ion-title>
+        <ion-title>Secretary App</ion-title>
       </ion-toolbar>
     </ion-header>
 
