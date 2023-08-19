0803a420691fefd4f44be0056e01166c1cb4505c
IdreesSindi <idresothman3@gmail.com>
2023-08-19 11:11:25 +0300

    test

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -11,7 +11,7 @@
 
       <div id="container">
         <strong>Ready to create an app?</strong>
-        <p>Hello2</p>
+        <p>Hello 22</p>
         <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
       </div>
     </ion-content>
