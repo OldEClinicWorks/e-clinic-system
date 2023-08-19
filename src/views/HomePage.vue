c2c98773a80d55cc1c6374069a2c31904d141659
IdreesSindi <idresothman3@gmail.com>
2023-08-19 10:46:56 +0300

    salar changed some things

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,7 +2,7 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>Dr App Changes by salar</ion-title>
       </ion-toolbar>
     </ion-header>
 
@@ -15,6 +15,7 @@
 
       <div id="container">
         <strong>Ready to create an app?</strong>
+        <p>Hello2</p>
         <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
       </div>
     </ion-content>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import legacy from '@vitejs/plugin-legacy'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-import { defineConfig } from 'vite'
+// import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
