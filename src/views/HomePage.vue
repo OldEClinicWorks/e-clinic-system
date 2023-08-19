635701ef66d57ccf7024cf89317dbdcd2bfc08a6
dev global <salarAdelSabry@gmail.com>
2023-08-19 10:56:56 +0300

    Changed Title

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,7 +2,7 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Dr App Changes by Idrees</ion-title>
+        <ion-title>Secretary App</ion-title>
       </ion-toolbar>
     </ion-header>
 
