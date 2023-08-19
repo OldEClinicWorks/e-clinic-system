17a3313805d6ae01c5c604fc8b856222582bb507
IdreesSindi <idresothman3@gmail.com>
2023-08-19 12:27:45 +0300

    layout

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -10,44 +10,37 @@
 
 
       <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Hello 22</p>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <ion-grid>
+          <ion-row>
+            <ion-col size="1">Menu</ion-col>
+            <ion-col size="2">left layout</ion-col>
+            <ion-col size="7">main page</ion-col>
+            <ion-col size="2">right layout</ion-col>
+          </ion-row>
+        </ion-grid>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCol, IonGrid, IonRow } from '@ionic/vue';
+
+
 </script>
 
 <style scoped>
 #container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
+
 }
 
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
 
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
 
-#container a {
-  text-decoration: none;
-}
+
+ion-col {
+    background-color: #135d54;
+    border: solid 1px #fff;
+    color: #fff;
+    text-align: center;
+  }
 </style>
