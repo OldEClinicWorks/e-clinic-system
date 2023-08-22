5e583087719e6a9d814340c70f178af55df4e52a
dev global <salarAdelSabry@gmail.com>
2023-08-22 07:53:46 +0300

    Added Queue, Cleaned Layout

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -49,39 +49,7 @@
               <XBaseRightSide>
                 <h2 class="text-xl font-semibold mb-2">Right Column Header</h2>
                 <div class="overflow-y-auto xbase-col-content-h xbase-col-content-h-right">
-                  RIGHT
-                  <div>lineeeeeeeee eeee eeee eeee eeee eeeeeeeeeeee eeee</div>
-                  <div>line</div>
-                  <div>line</div>
-                  <div>line</div>
-                  <div>line</div>
-                  <div>line</div>
-                  <div>line</div>
-                  <div>line</div>
-                  <div>line</div>
-                  <div>line</div>
-                  <div>line</div>
-                  <div>line</div>
-                  <div>line</div>
-                  <div>line</div>
-                  <div>line</div>
-                  <div>line</div>
-                  <div>line</div>
-                  <div>line</div>
-                  <div>line</div>
-                  <div>line</div>
-                  <div>line</div>
-                  <div>line</div>
-                  <div>line</div>
-                  <div>line</div>
-                  <div>line</div>
-                  <div>line</div>
-                  <div>line</div>
-                  <div>line</div>
-                  <div>line</div>
-                  <div>line</div>
-                  <div>line</div>
-                  <div>line</div>
+                  <Queue></Queue>
                 </div>
               </XBaseRightSide>
             </div>
@@ -105,6 +73,10 @@ import XBaseLeftSide from "@/views/layout/XBaseLeftSide.vue";
 import XBaseRightSide from "@/views/layout/XBaseRightSide.vue";
 import XBaseCenter from "@/views/layout/XBaseCenter.vue";
 import XBaseFooter from "@/views/layout/XBaseFooter.vue";
+
+
+// Queue
+import Queue from "@/views/components/queue/Queue.vue";
 
 </script>
 
