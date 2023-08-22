4d4ea041f03aac984cb70f8c63bc527aa456558a
dev global <salarAdelSabry@gmail.com>
2023-08-22 08:34:01 +0300

    Fixes

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { window } from '@tauri-apps/api';
 import '@ionic/vue/css/ionic.bundle.css';
 
 
+import { IonicVue } from '@ionic/vue';
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/vue/css/core.css';
 
